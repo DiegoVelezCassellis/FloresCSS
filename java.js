939392240fe001f7862792eef625d6ed82e7335c
PC1\inicio <diego.velez.cassellis@gmail.com>
2024-02-09 22:51:02 -0600

    [Flowes] La aparicion de las flores se modifico por el evento click de un boton

--- java.js
+++ java.js
@@ -1,6 +1,9 @@
-onload = () => {
+const btnInicio = document.querySelector('#btnInicial');
+
+btnInicio.addEventListener('click', function() {
   const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
+    document.querySelector('.flowers').classList.remove("not-loaded");
+    document.querySelector('#inital-box').classList.add("hide");
     clearTimeout(c);
   }, 1000);
-};
+});
